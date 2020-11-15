fa3eff14e847fa8966e1671ed8d2546a9fbaa260
Elena Borisova <elenaborrisova@gmail.com>
2020-11-15 20:03:23 +0200

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -1,12 +1,48 @@
 from docx import Document
 import os
+import tkinter
+import tkinter.filedialog
+import shutil
 
-mydir = 'C:\\Users\\User\\Desktop\\folder'
 
-for file in os.listdir(mydir):
-    document = Document(mydir + "\\" + file)
-    name = document.paragraphs[2].text
+def choose_directory():
+    root = tkinter.Tk()
+    root.withdraw()
+    dir_name = tkinter.filedialog.askdirectory(parent=root, initialdir="/", title='Please select a directory')
+    if len(dir_name) > 0:
+        print(f"You chose {dir_name}")
 
-    old_file = os.path.join(mydir, file)
-    new_file = os.path.join(mydir, f"{name}.docx")
-    os.rename(old_file, new_file)
+    return dir_name
+
+
+def create_new_dir(chosen_dir):
+    new_dir = os.path.join(chosen_dir, "Renamed Documents")
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
+
+    return new_dir
+
+
+def copy_all_files(chosen_dir, new_dir):
+    all_files = os.listdir(chosen_dir)
+
+    for file in all_files:
+        full_file_name = os.path.join(chosen_dir, file)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, new_dir)
+
+
+def rename_files_in_dir(new_dir):
+    for file in os.listdir(new_dir):
+        document = Document(new_dir + "\\" + file)
+        name = document.paragraphs[2].text
+
+        old_file_name = os.path.join(new_dir, file)
+        new_file_name = os.path.join(new_dir, f"{name}.docx")
+        os.rename(old_file_name, new_file_name)
+
+
+chosen_dir = choose_directory()
+new_dir = create_new_dir(chosen_dir)
+copy_all_files(chosen_dir, new_dir)
+rename_files_in_dir(new_dir)
